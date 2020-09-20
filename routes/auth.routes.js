b1b81f58e6d4a32e3978d29b429d4b9d3cbec894
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-09-20 12:18:29 +0300

    create mongo DB

---
+++ routes/auth.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const router = Router();
+
+router.post("/register", async (req, res) => {
+    try {
+    } catch (e) {}
+});
+
+router.post("/login", async (req, res) => {});
+
+module.exports = router;
